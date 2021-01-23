6af1e232a4a3fa381590b8473214e84d7d2f0b44
Esteban4 <juanes.x777@gmail.com>
2021-01-22 23:32:50 -0500

    Cambios en Facturas y detalles de facturas.

---
+++ WebApplicationFacturas/WebApplicationFacturas/DTO/Requests/FacturasProductosBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationFacturas.DTO.Requests
+{
+    public class FacturasProductosBase
+    {
+        public int FacturaId { get; set; }
+        public int ProductosId { get; set; }
+        public string NombreProducto { get; set; }
+        public int Cantidad { get; set; }
+        public float Precio { get; set; }
+    }
+}
+
